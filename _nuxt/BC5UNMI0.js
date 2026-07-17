fe084b1566b0fd660e8152ceecfe3771d5f245d1
Peter267 <Peter267@users.noreply.github.com>
2026-07-17 03:39:23 +0000

    deploy: 61236a14d78a195dd06b306897a231933c1555eb

---
+++ _nuxt/BC5UNMI0.js
@@ -0,0 +1,1 @@
+import{v as a,x as s,y as r,z as u,A as o}from"./B-m7p67f.js";function h(n){const e=n||s();return e.ssrContext?.head||e.runWithContext(()=>{if(r()){const t=u(o);if(!t)throw new Error("[nuxt] [unhead] Missing Unhead instance.");return t}})}function c(n,e={}){const t=e.head||h(e.nuxt);return a(n,{head:t,...e})}export{c as u};
